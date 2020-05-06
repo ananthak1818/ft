994766dc9582a6b90421a1cd14ff0a736e3c8f2d
ananthak1818 <ananth18ananth@gmail.com>
2020-05-06 20:49:03 +0530

    upto261

---
+++ app/models/stock.rb
@@ -0,0 +1,11 @@
+class Stock < ApplicationRecord
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new(publishable_token: 'Tpk_a2a4b0a6c139456491af81957a10e6a7',
+                                    endpoint: 'https://sandbox.iexapis.com/v1')
+        begin
+            new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+        rescue => exception
+            return nil
+        end                                    
+    end
+end
